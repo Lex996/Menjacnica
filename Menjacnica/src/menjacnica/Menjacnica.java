d386086f4aacdf146d27bb959fb7c9208349593f
Lex996 <rakicaleksandra1@gmail.com>
2017-03-29 23:49:02 +0200

    U okviru klase Menjacnica implementirane metode dodajKurs, obrisiKurs i nadjiKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -6,28 +6,50 @@ import java.util.LinkedList;
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-	
+
 	private LinkedList<Valuta> valute;
 
 	@Override
 	public void dodajKurs(Kurs k, Valuta v) {
-	
-		
+		for (Valuta valuta : valute) {
+			if (valuta.equals(v)) {
+				v.getKursevi().add(k);
+				return;
+			}
+		}
+
 	}
 
 	@Override
 	public Kurs obrisiKurs(Kurs k, Valuta v) {
-		
-		return null;
+		for (Valuta valuta : valute) {
+			if (valuta.equals(v)) {
+				for (Kurs kurs : v.getKursevi()) {
+					if (kurs.equals(k))
+					v.getKursevi().remove(kurs);
+				}
+
+			}
+		}
+
+		return k;
 	}
 
 	@Override
 	public Kurs nadjiKurs(GregorianCalendar datum, Valuta v) {
-		
+		for (Valuta valuta : valute) {
+			if (valuta.equals(v)) {
+
+				for (Kurs kurs : v.getKursevi()) {
+					if (kurs.getDatum().equals(datum))
+						return kurs;
+					
+				}
+
+			}
+		}
+
 		return null;
 	}
-	
-	
-	
 
 }
